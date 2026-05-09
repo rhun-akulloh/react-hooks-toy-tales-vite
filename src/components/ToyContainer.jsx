b50e650d1caddeb3f8c52e782e8c878ee0fb06cb
rhun-akulloh <rhunakulloh@gmail.com>
2026-05-09 19:16:45 +0300

    feat: like toy via like button
    - Add handleUpdateToy function in App component
    - Pass onUpdateToy callback to ToyContainer and ToyCard
    - Implement PATCH request to backend to increase likes
    - Update toy state while maintaining order in toys array
    - Increment likes count on button click

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys, onDeleteToy }) {
+function ToyContainer({ toys, onDeleteToy, onUpdateToy }) {
   // Render individual ToyCard components for each toy in the toys array
   return (
     <div id="toy-collection">
       {toys.map((toy) => (
-        <ToyCard key={toy.id} toy={toy} onDeleteToy={onDeleteToy} />
+        <ToyCard key={toy.id} toy={toy} onDeleteToy={onDeleteToy} onUpdateToy={onUpdateToy} />
       ))}
     </div>
   );
